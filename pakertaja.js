5cce2138d4b899880cc68a0103abe6a8e0ad7103
Rauli Laine <rauli.laine@iki.fi>
2016-11-06 21:52:19 +0200

    Add initial version of the library

---
+++ pakertaja.js
@@ -0,0 +1,177 @@
+;(function (global, factory) {
+  "use strict";
+  var Pakertaja = factory(global);
+
+  if (typeof module === "object" && module != null && module.exports) {
+    module.exports = Pakertaja;
+  }
+  else if (typeof define === "function" && define.amd) {
+    define(function () { return Pakertaja; });
+  } else {
+    var original = global.p;
+
+    Pakertaja.noConflict = function () {
+      return original;
+    };
+    global.p = global.Pakertaja = Pakertaja;
+  }
+})(typeof window !== "undefined" ? window : this, function (global) {
+  "use strict";
+  var document = global.document;
+  var entityMapping = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&#39;",
+    "/": "&#x2f;",
+  };
+
+  /**
+   * Tests whether given value is string.
+   */
+  function isString(value) {
+    return typeof value === "string";
+  }
+
+  /**
+   * Tests whether given value is function.
+   */
+  function isFunction(value) {
+    return typeof value === "function";
+  }
+
+  /**
+   * Tests whether given value is HTML element.
+   */
+  function isElement(value) {
+    return typeof value === "object" && value != null && value.nodeType === 1;
+  }
+
+  /**
+   * Converts given value into string, and if it's a function, calls the
+   * function with given this object and returns value returned by that
+   * function.
+   */
+  function toStringWithCallback(thisObject, value) {
+    if (isFunction(value)) {
+      value = value.call(thisObject);
+    }
+
+    return value != null ? String(value) : "";
+  }
+
+  function applyStyleProperties(node, properties) {
+    if (isFunction(properties)) {
+      properties = properties.call(node);
+    }
+    if (isString(properties)) {
+      node.setAttribute("style", properties);
+    }
+    else if (properties != null) {
+      Object.keys(properties).forEach(function (key) {
+        node.style[key] = toStringWithCallback(node, properties[key]);
+      });
+    }
+  }
+
+  function Pakertaja() {
+    var node = document.createElement(arguments[0]);
+
+    for (var i = 1, length = arguments.length; i < length; ++i) {
+      var arg = arguments[i];
+
+      if (isString(arg)) {
+        node.textContent = arg;
+      }
+      else if (isElement(arg)) {
+        node.appendChild(arg);
+      }
+      else if (arg != null) {
+        Object.keys(arg).forEach(function (key) {
+          var value = arg[key];
+
+          if (key === "text") {
+            node.textContent = toStringWithCallback(node, value);
+          }
+          else if (key === "html") {
+            node.innerHTML = toStringWithCallback(node, value);
+          }
+          else if (key === "style") {
+            applyStyleProperties(node, value);
+          }
+          else if (/^on.+$/.test(key)) {
+            node.addEventListener(key.substring(2), function (ev) {
+              value.call(node, ev);
+            });
+          } else {
+            node.setAttribute(key, toStringWithCallback(node, value));
+          }
+        });
+      }
+    }
+
+    return node;
+  }
+
+  Pakertaja.escape = function (input) {
+    return String(input).replace(/[&<>"'\/]/g, function (s) {
+      return entityMapping[s];
+    });
+  };
+
+  [
+    // The root element
+    "html",
+
+    // Document metadata
+    "head", "title", "base", "link", "meta", "style",
+
+    // Scripting
+    "script", "noscript",
+
+    // Sections
+    "body", "section", "nav", "article", "aside", "h1", "h2", "h3", "h4",
+    "h5", "h6", "header", "footer", "address",
+
+    // Grouping content
+    "p", "hr", "br", "pre", "dialog", "blockquote", "ol", "ul", "li", "dl",
+    "dt", "dd",
+
+    // Text level semantics
+    "a", "q", "cite", "em", "strong", "small", "mark", "dfn", "abbr",
+    "time", "progress", "meter", "code", "var", "samp", "kbd", "sub",
+    "sup", "span", "i", "b", "bdo", "ruby", "rt", "rp",
+
+    // Edits
+    "ins", "del",
+
+    // Embedded content
+    "figure", "img", "iframe", "embed", "object", "param", "video", "audio",
+    "source", "canvas", "map", "area",
+
+    // Tabular data
+    "table", "caption", "colgroup", "col", "tbody", "thead", "tfoot", "tr",
+    "td", "th",
+
+    // Forms
+    "form", "fieldset", "label", "input", "button", "select", "datalist",
+    "optgroup", "option", "textarea", "output",
+
+    // Interactive elements
+    "details", "command", "bb", "menu",
+
+    // Miscanellaous elements
+    "legend", "div"
+  ].forEach(function (tag) {
+    Pakertaja[tag] = function () {
+      var args = [tag];
+
+      Array.prototype.push.apply(args, arguments);
+
+      return Pakertaja.apply(null, args);
+    };
+  });
+
+  return Pakertaja;
+});
