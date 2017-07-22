c547590ffd74f1b21bc9ed8e205e0eff37964ec8
Rauli Laine <rauli.laine@iki.fi>
2017-07-22 10:17:56 +0300

    Do not include generated files in the repository
    Remove automatically generated files from the repository and switch to
    Babili for minifying.

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,20 @@
-"use strict";
-const gulp = require("gulp");
-const uglify = require("gulp-uglify");
-const sourcemaps = require("gulp-sourcemaps");
-const rename = require("gulp-rename");
-const pump = require("pump");
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const babili = require('gulp-babili');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
 
-gulp.task("compress", function (cb) {
-  pump(
-    [
-      gulp.src("pakertaja.js"),
-      sourcemaps.init(),
-      uglify(),
-      rename("pakertaja.min.js"),
-      sourcemaps.write("."),
-      gulp.dest(".")
-    ],
-    cb
-  );
+gulp.task('minify', () => {
+  return gulp.src('./pakertaja.js')
+    .pipe(plumber())
+    .pipe(babili())
+    .pipe(rename('./pakertaja.min.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('.'));
 });
 
-gulp.task("watch", ["compress"], function () {
-  gulp.watch("pakertaja.js", ["compress"]);
+gulp.task('watch', ['minify'], () => {
+  gulp.watch('./pakertaja.js', ['minify']);
 });
 
-gulp.task("default", ["compress"]);
+gulp.task('default', ['minify']);
