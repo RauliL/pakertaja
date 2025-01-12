d221d370d634d12421ef3133dd1a78e867de2712
Rauli Laine <rauli.laine@iki.fi>
2025-01-12 22:48:09 +0200

    Add more attribute documentation

--- index.d.ts
+++ index.d.ts
@@ -887,7 +887,20 @@ type MeterAttributes = Attributes & {
 };
 
 type ModAttributes = Attributes & {
+  /**
+   * This attribute defines the URI of a resource that explains the change,
+   * such as a link to meeting minutes or a ticket in a troubleshooting system.
+   */
   cite?: StringAttribute;
+  /**
+   * This attribute indicates the time and date of the change and must be a
+   * valid date with an optional time string. If the value cannot be parsed
+   * as a date with an optional time string, the element does not have an
+   * associated timestamp. For the format of the string without a time, see
+   * Format of a valid date string. The format of the string if it includes
+   * both date and time is covered in Format of a valid local date and time
+   * string.
+   */
   datetime?: StringAttribute;
 };
 
