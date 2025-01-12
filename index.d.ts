bc28b5ebc43153f8dadb9283c02c18a24163a0b6
Rauli Laine <rauli.laine@iki.fi>
2025-01-12 23:18:06 +0200

    Add more attribute documentation

--- index.d.ts
+++ index.d.ts
@@ -905,19 +905,146 @@ type ModAttributes = Attributes & {
 };
 
 type ImgAttributes = Attributes & {
+  /**
+   * Defines text that can replace the image in the page.
+   */
   alt?: StringAttribute;
+  /**
+   * Indicates if the fetching of the image must be done using a CORS request.
+   * Image data from a CORS-enabled image returned from a CORS request can be
+   * reused in the <canvas> element without being marked "tainted".
+   *
+   * If the crossorigin attribute is not specified, then a non-CORS request is
+   * sent (without the Origin request header), and the browser marks the image
+   * as tainted and restricts access to its image data, preventing its usage in
+   * <canvas> elements.
+   *
+   * If the crossorigin attribute is specified, then a CORS request is sent
+   * (with the Origin request header); but if the server does not opt into
+   * allowing cross-origin access to the image data by the origin site (by not
+   * sending any Access-Control-Allow-Origin response header, or by not
+   * including the site's origin in any Access-Control-Allow-Origin response
+   * header it does send), then the browser blocks the image from loading, and
+   * logs a CORS error to the devtools console.
+   */
   crossorigin?: StringAttribute;
+  /**
+   * This attribute provides a hint to the browser as to whether it should
+   * perform image decoding along with rendering the other DOM content in a
+   * single presentation step that looks more "correct" (sync), or render
+   * and present the other DOM content first and then decode the image and
+   * present it later (async). In practice, async means that the next paint
+   * does not wait for the image to decode.
+   *
+   * It is often difficult to perceive any noticeable effect when using
+   * decoding on static <img> elements. They'll likely be initially rendered
+   * as empty images while the image files are fetched (either from the
+   * network or from the cache) and then handled independently anyway, so
+   * the "syncing" of content updates is less apparent. However, the blocking
+   * of rendering while decoding happens, while often quite small, can be
+   * measured — even if it is difficult to observe with the human eye. See
+   * What does the image decoding attribute actually do? for a more detailed
+   * analysis (tunetheweb.com, 2023).
+   *
+   * Using different decoding types can result in more noticeable differences
+   * when dynamically inserting <img> elements into the DOM via JavaScript
+   * — see HTMLImageElement.decoding for more details.
+   */
   decoding?: StringAttribute<"sync" | "async" | "auto">;
+  /**
+   * Marks the image for observation by the PerformanceElementTiming API.
+   * The value given becomes an identifier for the observed image element.
+   * See also the elementtiming attribute page.
+   */
   elementtiming?: StringAttribute;
+  /**
+   * Provides a hint of the relative priority to use when fetching the image.
+   */
   fetchpriority?: StringAttribute<FetchPriority>;
+  /**
+   * The intrinsic height of the image, in pixels. Must be an integer without
+   * a unit.
+   */
   height?: NumberAttribute;
+  /**
+   * This Boolean attribute indicates that the image is part of a server-side
+   * map. If so, the coordinates where the user clicked on the image are sent
+   * to the server.
+   */
   ismap?: BooleanAttribute;
+  /**
+   * Indicates how the browser should load the image.
+   */
   loading?: StringAttribute<"eager" | "lazy">;
+  /**
+   * A string indicating which referrer to use when fetching the resource.
+   */
   referrerpolicy?: StringAttribute<ReferrerPolicy>;
+  /**
+   * One or more strings separated by commas, indicating a set of source sizes.
+   * Each source size consists of:
+   *
+   * - A media condition. This must be omitted for the last item in the list.
+   * - A source size value.
+   *
+   * Media Conditions describe properties of the viewport, not of the image.
+   * For example, (max-height: 500px) 1000px proposes to use a source of
+   * 1000px width, if the viewport is not higher than 500px.
+   *
+   * Source size values specify the intended display size of the image. User
+   * agents use the current source size to select one of the sources supplied
+   * by the srcset attribute, when those sources are described using width
+   * (w) descriptors. The selected source size affects the intrinsic size of
+   * the image (the image's display size if no CSS styling is applied). If
+   * the srcset attribute is absent, or contains no values with a width
+   * descriptor, then the sizes attribute has no effect.
+   */
   sizes?: StringAttribute;
+  /**
+   * The image URL. Mandatory for the <img> element. On browsers supporting
+   * srcset, src is treated like a candidate image with a pixel density
+   * descriptor 1x, unless an image with this pixel density descriptor is
+   * already defined in srcset, or unless srcset contains w descriptors.
+   */
   src?: StringAttribute;
+  /**
+   * One or more strings separated by commas, indicating possible image sources
+   * for the user agent to use. Each string is composed of:
+   *
+   * - A URL to an image
+   *   Optionally, whitespace followed by one of:
+   *   A width descriptor (a positive integer directly followed by w). The width
+   *   descriptor is divided by the source size given in the sizes attribute to
+   *   calculate the effective pixel density.
+   *   A pixel density descriptor (a positive floating point number directly
+   *   followed by x).
+   *
+   * If no descriptor is specified, the source is assigned the default descriptor
+   * of 1x.
+   *
+   * It is incorrect to mix width descriptors and pixel density descriptors
+   * in the same srcset attribute. Duplicate descriptors (for instance, two
+   * sources in the same srcset which are both described with 2x) are also
+   * invalid.
+   *
+   * If the srcset attribute uses width descriptors, the sizes attribute must
+   * also be present, or the srcset itself will be ignored.
+   *
+   * The user agent selects any of the available sources at its discretion.
+   * This provides them with significant leeway to tailor their selection
+   * based on things like user preferences or bandwidth conditions. See our
+   * Responsive images tutorial for an example.
+   */
   srcset?: StringAttribute;
+  /**
+   * The intrinsic width of the image in pixels. Must be an integer without a
+   * unit.
+   */
   width?: NumberAttribute;
+  /**
+   * The partial URL (starting with #) of an image map associated with the
+   * element.
+   */
   usemap?: StringAttribute;
 };
 
