9d4c1c8a7b8f79ac27ac3becc01e744a82b5a0fa
Rauli Laine <rauli.laine@iki.fi>
2025-01-12 22:45:33 +0200

    Add more attribute documentation

--- index.d.ts
+++ index.d.ts
@@ -803,21 +803,86 @@ type AnchorAttributes = Attributes & {
 };
 
 type TimeAttributes = Attributes & {
+  /**
+   * This attribute indicates the time and/or date of the element and must
+   * be in one of the formats described below.
+   */
   datetime?: StringAttribute;
 };
 
 type ProgressAttributes = Attributes & {
+  /**
+   * This attribute describes how much work the task indicated by the progress
+   * element requires. The max attribute, if present, must have a value
+   * greater than 0 and be a valid floating point number. The default value is
+   * 1.
+   */
   max?: NumberAttribute;
+  /**
+   * This attribute specifies how much of the task that has been completed. It
+   * must be a valid floating point number between 0 and max, or between 0 and
+   * 1 if max is omitted. If there is no value attribute, the progress bar is
+   * indeterminate; this indicates that an activity is ongoing with no
+   * indication of how long it is expected to take.
+   */
   value?: NumberAttribute;
 };
 
 type MeterAttributes = Attributes & {
+  /**
+   * The current numeric value. This must be between the minimum and maximum
+   * values (min attribute and max attribute) if they are specified. If
+   * unspecified or malformed, the value is 0. If specified, but not within
+   * the range given by the min attribute and max attribute, the value is
+   * equal to the nearest end of the range.
+   */
   value?: NumberAttribute;
+  /**
+   * The lower numeric bound of the measured range. This must be less than
+   * the maximum value (max attribute), if specified. If unspecified, the
+   * minimum value is 0.
+   */
   min?: NumberAttribute;
+  /**
+   * The upper numeric bound of the measured range. This must be greater than
+   * the minimum value (min attribute), if specified. If unspecified, the
+   * maximum value is 1.
+   */
   max?: NumberAttribute;
+  /**
+   * The upper numeric bound of the low end of the measured range. This must
+   * be greater than the minimum value (min attribute), and it also must be
+   * less than the high value and maximum value (high attribute and max
+   * attribute, respectively), if any are specified. If unspecified, or if
+   * less than the minimum value, the low value is equal to the minimum value.
+   */
   low?: NumberAttribute;
+  /**
+   * The lower numeric bound of the high end of the measured range. This must
+   * be less than the maximum value (max attribute), and it also must be
+   * greater than the low value and minimum value (low attribute and min
+   * attribute, respectively), if any are specified. If unspecified, or if
+   * greater than the maximum value, the high value is equal to the maximum
+   * value.
+   */
   high?: NumberAttribute;
+  /**
+   * This attribute indicates the optimal numeric value. It must be within the
+   * range (as defined by the min attribute and max attribute). When used with
+   * the low attribute and high attribute, it gives an indication where along
+   * the range is considered preferable. For example, if it is between the min
+   * attribute and the low attribute, then the lower range is considered
+   * preferred. The browser may color the meter's bar differently depending on
+   * whether the value is less than or equal to the optimum value.
+   */
   optimum?: NumberAttribute;
+  /**
+   * This optional attribute is used to explicitly set a <form> owner for the
+   * <meter> element. If omitted, the <meter> is associated with its ancestor
+   * <form> element or the form association set by the form attribute on
+   * another ancestor element, such as on a <fieldset>, if any. If included,
+   * the value must be the id of a <form> in the same tree.
+   */
   form?: StringAttribute;
 };
 
