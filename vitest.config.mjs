8f7fd0235365f3d0ff9d3760f6876b5cba0575c9
Rauli Laine <rauli.laine@iki.fi>
2026-09-07 09:29:28 +0300

    Switch to v8 coverage reporter

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      provider: "istanbul",
+      provider: "v8",
       reporter: ["lcov", "text"],
     },
     environment: "happy-dom",
